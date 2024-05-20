7e3fc975745510d5e2311743401c0ab3a51fee09
feoolga <kirianova_ov@mail.ru>
2024-05-20 21:53:28 +0300

    crm

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,16 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: { layout: 'main' },
+    component: () => import('../views/Categories.vue')
   }
 ]
 
